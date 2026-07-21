42cc05fd490fa616212585ee0cf1e2ed05da3d09
PythonScriptor64 <139090143+PythonScriptor64@users.noreply.github.com>
2026-07-21 05:18:50 -0700

    building db stuff

--- db_manager.py
+++ db_manager.py
@@ -1,22 +1,57 @@
 import sqlmodel
 import sqlite3
+import uuid
+import time
 import os
 
 _engine = sqlmodel.create_engine("sqlite:///main.db")
 
-class User(sqlmodel.SQLModel, table=True):
-    id: int | None = sqlmodel.Field(default=None, primary_key=True)
-    period1: str
-    period2: str
-    period3: str
-    period4: str
-    period5: str
-    period6: str
+def int_time(): return int(time.time())
+def str_uuid4(): return str(uuid.uuid4())
 
+class User(sqlmodel.SQLModel, table=True):
+    uid: int = sqlmodel.Field(primary_key=True) # required
+    period1: str | None = None
+    period2: str | None = None
+    period3: str | None = None
+    period4: str | None = None
+    period5: str | None = None
+    period6: str | None = None
+
+class EventDate(sqlmodel.SQLModel, table=True):
+    event_uuid: str = sqlmodel.Field(primary_key=True, default_factory=str_uuid4)
+    span: bool = False
+    start_time: int # required
+    end_time: int | None = None
+    name: str = "Event Title"
+    description: str = "No event description provided"
+    approved: bool = False
+    created_by: int | None = None
+    created_at: int = sqlmodel.Field(default_factory=int_time)
+    approved_by: int | None = None
+    approved_at: int | None = None
+
+class Rule(sqlmodel.SQLModel, table=True):
+    rule_uuid: str = sqlmodel.Field(primary_key=True, default_factory=str_uuid4)
+    rule_number: int = sqlmodel.Field(unique=True, index=True)
+    title: str = "Rule Title"
+    description: str = "No rule description provided"
+    created_by: int | None = None
+    created_at: int = sqlmodel.Field(default_factory=int_time)
+
+class GenericValues(sqlmodel.SQLModel, table=True):
+    key: str = sqlmodel.Field(primary_key=True) # required
+    value: str | None = None
+
+# generic db operations
 def backupdb():
-    os.mkdir("db_backups")
+    os.makedirs("db_backups", exist_ok=True)
     # write this later
     
+def cleanup():
+    __import__("time").sleep(7)
+
+# schedule management
 def save_user_schedule(schedule: dict[int, str | None], user_id: int):
     print(f"Schedule: {schedule}\n\nUser ID: {user_id}")
     # write this later
@@ -26,7 +61,13 @@ def fetch_user_schedule(user_id: int):
     # write this later
     # return {}
 
-def cleanup():
-    __import__("time").sleep(7)
+# event management
+pass
+
+# rule management
+def get_greatest_rule_number():
+    pass
+
+
 
 sqlmodel.SQLModel.metadata.create_all(_engine)
