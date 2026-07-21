6efea360f7342e31441f1b677a457b4ff0c56085
PythonScriptor64 <139090143+PythonScriptor64@users.noreply.github.com>
2026-07-21 05:18:29 -0700

    testing modals

--- schedulemodaltest1.py
+++ schedulemodaltest1.py
@@ -19,12 +19,19 @@ for course in courses:
 
 class CourseSelectModal(discord.ui.Modal):
     def __init__(self):
-        super().__init__(title="## Select your courses\n-# Leave a slot blank to keep existing selection")
+        super().__init__(title="Select your courses(DEVEL)")
 
-        for period in range(1,7):
-            period_dropdown_ar = discord.ui.ActionRow()
-            period_dropdown_ar.add_item(PeriodDropdown(self, period))
-            self.add_item(period_dropdown_ar)
+        # self.add_item(discord.ui.TextDisplay(content="-# Leave a slot blank to keep existing selection"))
+        for period in range(1,6):
+            # period_dropdown_ar = discord.ui.ActionRow()
+            # period_dropdown_ar.add_item(PeriodDropdown(self, period))
+            # self.add_item(period_dropdown_ar)
+            self.add_item(discord.ui.Label(text="dropdown",component=PeriodDropdown(self, period)))
+            # self.add_item()
+
+        # self.add_item(discord.ui.TextInput(label="yo", placeholder="placeholder here"))
+
+        # self.add_item(discord.ui.Select(options=options))
 
 
 
