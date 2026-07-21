52ca36445f5dca41b83026db7d16a64539d7113a
PythonScriptor64 <139090143+PythonScriptor64@users.noreply.github.com>
2026-07-21 10:00:31 -0700

    made the ui a lil nicer

--- schedulemodaltest1.py
+++ schedulemodaltest1.py
@@ -19,14 +19,14 @@ for course in courses:
 
 class CourseSelectModal(discord.ui.Modal):
     def __init__(self):
-        super().__init__(title="Select your courses(DEVEL)")
+        super().__init__(title="Select your courses(Development)")
 
         # self.add_item(discord.ui.TextDisplay(content="-# Leave a slot blank to keep existing selection"))
         for period in range(1,6):
             # period_dropdown_ar = discord.ui.ActionRow()
             # period_dropdown_ar.add_item(PeriodDropdown(self, period))
             # self.add_item(period_dropdown_ar)
-            self.add_item(discord.ui.Label(text="dropdown",component=PeriodDropdown(self, period)))
+            self.add_item(discord.ui.Label(text=f"Period {period}",component=PeriodDropdown(self, period)))
             # self.add_item()
 
         # self.add_item(discord.ui.TextInput(label="yo", placeholder="placeholder here"))
@@ -40,7 +40,7 @@ class CourseSelectModal(discord.ui.Modal):
 
 class PeriodDropdown(discord.ui.Select):
     def __init__(self, parent_view: CourseSelectModal, period: int):
-        super().__init__(options=options,placeholder=f"Period {period}")
+        super().__init__(options=options,placeholder=f"Period {period}", required=False)
         self.period = period
         self.parent_view = parent_view
 
