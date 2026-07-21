adeae845c357b2ec331d08ce495067ee9f90e1c1
PythonScriptor64 <139090143+PythonScriptor64@users.noreply.github.com>
2026-07-21 08:24:11 -0700

    modal testing

--- modal1test.py
+++ modal1test.py
@@ -1,4 +1,19 @@
 import discord
+import logging
+import json
+
+logger = logging.getLogger(__name__)
+
+try:
+    with open("courses.json","r") as courses_json_f:
+        courses = json.load(courses_json_f)
+except (FileNotFoundError, json.JSONDecodeError):
+    logger.error("courses.json is missing or invalid")
+    courses = {None: {"name":"courses.json is missing or damaged, contact an administrator"}}
+
+options = []
+for course in courses:
+    options.append(discord.SelectOption(label=f"{courses[course].get('name', course)}", value=f"{course}"))
 
 
 class ModalTest(discord.ui.Modal):
@@ -16,4 +31,6 @@ class ModalTest(discord.ui.Modal):
         # self.add_item(container)
 
         self.add_item(discord.ui.TextDisplay(content="test"))
+
+        self.add_item(discord.ui.Label(text="yo this is label", component=discord.ui.Select(options=options)))
 
